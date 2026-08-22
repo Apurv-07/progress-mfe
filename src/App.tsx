cb90d114dc37cdbfe0198b48597d257b0e2f4d66
Apurv-07 <mailapurva007@gmail.com>
2026-08-23 00:18:09 +0530

    Added the personal progress mfe for user

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen p-10">
+      <h1 className="text-4xl font-bold">
+        Progress MFE
+      </h1>
+
+      <p className="mt-4">
+        This application is running independently.
+      </p>
+    </div>
+  );
+}
+
+export default App;
